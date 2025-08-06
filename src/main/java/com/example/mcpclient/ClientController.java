b963eb2518a54d6189122df3a257bc4e42f94f4b
anastasiia <anastasiia.peleshok@gmail.com>
2025-08-06 10:51:39 +0000

    Added streamable endpoint

--- src/main/java/com/example/mcpclient/ClientController.java
+++ src/main/java/com/example/mcpclient/ClientController.java
@@ -8,16 +8,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
 
 @Controller
 @RequiredArgsConstructor
 public class ClientController {
     private final ChatClient chatClient;
 
-    @PostMapping(value = "/ask", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @PostMapping(value = "/askStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @ResponseBody
-    public Flux<String> ask(@RequestParam String userInput) {
+    public Flux<String> askStream(@RequestParam String userInput) {
         return chatClient.prompt(userInput).stream().content();
+    }
+
+    @PostMapping("/ask")
+    @ResponseBody
+    public Mono<String> ask(@RequestParam String userInput) {
+        return Mono.fromCallable(() ->
+                        chatClient.prompt(userInput)
+                                .call()
+                                .content()
+                ).subscribeOn(Schedulers.boundedElastic())
+                .timeout(Duration.ofSeconds(60));
     }
 
 }
